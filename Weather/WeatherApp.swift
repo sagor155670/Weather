043c136f932878a3535c47cac389e96c7b5b7616
sagor155670 <sagor@braincraftapps.com>
2023-09-18 16:53:53 +0600

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Saiful Islam Sagor on 18/9/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
